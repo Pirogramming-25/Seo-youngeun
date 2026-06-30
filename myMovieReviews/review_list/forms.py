374e329894c3d5ceb00559228f23eb2276db29ce
dahliare <dahliare02@gmail.com>
2026-06-30 23:39:25 +0900

    나만의 영화 기록 사이트 완성

--- myMovieReviews/review_list/forms.py
+++ myMovieReviews/review_list/forms.py
@@ -14,3 +14,14 @@ class PostForm(forms.ModelForm):
             "running_time",
             "review_content",
         ]
+
+        labels = {
+            "title": "제목",
+            "release_year": "개봉년도",
+            "director": "감독",
+            "actors": "주연",
+            "genre": "장르",
+            "rating": "별점",
+            "running_time": "러닝타임",
+            "review_content": "리뷰",
+        }
